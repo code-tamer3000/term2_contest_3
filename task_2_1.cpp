af26cc9cd59b4a28192a1d3c3d4ce414a8c40165
code-tamer3000 <parfenov-art@mail.ru>
2023-05-10 13:14:03 +0500

    сделал изменеия

--- task_2_1.cpp
+++ task_2_1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<size_t> get_pi() {
+std::vector<size_t> pi() {
    std::vector<size_t> ans;
    size_t tmp = 0;
    while (std::cin >> tmp) {
@@ -10,26 +10,26 @@ std::vector<size_t> get_pi() {
    return ans;
 }
 
-std::string find_the_string(const std::vector<size_t>& pi) {
+std::string find_min_string_by_pi(const std::vector<size_t>& pi) {
     std::string ans = "a";
     for (size_t i = 1; i < pi.size(); i++) {
         if (pi[i] != 0) ans.push_back(ans[pi[i] - 1]);
         else { 
             size_t ind = pi[i - 1];
-            std::vector<bool> alfabet(26);
+            std::vector<bool> alphabet(26);
             while (ind > 0) {
-                alfabet[ans[ind] - 'a'] = true;
+                alphabet[ans[ind] - 'a'] = true;
                 ind = pi[ind - 1];
             }
             char next = 'b';
-            while (alfabet[next - 'a']) next++;
+            while (alphabet[next - 'a']) next++;
             ans.push_back(next);
         }
     }
     return ans;
 }
 
-int  main() {
-    std::cout << find_the_string(get_pi()) << '\n';
+int main() {
+    std::cout << find_min_string_by_pi(pi()) << '\n';
     return 0;
 }
